456c9442bd8702749565edd71deb27cbcfe8aa56
IMVinayak3 <123053249+StellarXVA3@users.noreply.github.com>
2025-06-03 22:18:08 +0530

    Navigation Baseline

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-const navItems = document.querySelectorAll("li.nav-item");
+const navItems = document.querySelectorAll("li.nav-item>a");
 
 document.addEventListener("DOMContentLoaded", () => {
   navItems.forEach((navItem) => {
     let letters = navItem.textContent.toUpperCase().split("");
     navItem.textContent = "";
 
+    let i = 0;
     letters.forEach((letter) => {
       let span = document.createElement("span");
       span.textContent = letter;
       span.setAttribute("data-char", letter);
+      span.style.setProperty("--transiton-delay-navItem-ltr", `${i * 69}ms`);
       navItem.appendChild(span);
+      i++;
     });
   });
 });
